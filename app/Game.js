18edb2245e8bf40127e95d89fbcfe387539a8f62
Joanna Borkala-Kilian <joanna.kilian@foundbycode.dev>
2022-01-08 15:48:40 +0100

    create structure and start

--- app/Game.js
+++ app/Game.js
@@ -1,6 +1,26 @@
 class Game {
-
+    constructor({
+        lettersWrapper, 
+        categoryWrapper, 
+        wordWrapper, 
+        outputWrapper
+    }) {
+        this.lettersWrapper = lettersWrapper;
+        this.categoryWrapper = categoryWrapper;
+        this.wordWrapper = wordWrapper;
+        this.outputWrapper = outputWrapper;
+    }
+    start() {
+        for(let i=0; i++; i<26) {
+            const label = (i+10).toString(36);
+            console.log(label);
+        }
+    }
 }
-
-const game = new Game();
-game.start
+const game = new Game({
+    lettersWrapper: document.getElementById('letters'),
+    categoryWrapper: document.getElementById('category'),
+    wordWrapper: document.getElementById('word'),
+    outputWrapper: document.getElementById('output')
+});
+game.start();
